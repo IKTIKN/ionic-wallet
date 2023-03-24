bd1a8ca43d785457abddd13741cecd660d5842ad
IKTIKN <developermarc@outlook.com>
2023-03-24 02:14:42 +0100

    Bitcoin service

--- src/app/services/bitcoin.service.ts
+++ src/app/services/bitcoin.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import * as bitcoin from 'bitcoinjs-lib';
 import { Network } from 'bitcoinjs-lib';
-import ECPairFactory, { ECPairInterface } from 'ecpair';
+import { ECPairFactory, ECPairInterface } from 'ecpair';
 import * as ecc from 'tiny-secp256k1';
+import * as bip39 from 'bip39';
+import { BIP32Factory, BIP32Interface } from 'bip32';
 
 const ECPair = ECPairFactory(ecc);
+const bip32 = BIP32Factory(ecc);
 
 export interface KeyPair {
     address: string;
     wif: string;
+}
+
+export interface Wallet {
+    addressType: string;
+    keyPair: KeyPair;
+    balance: number;
+    balanceFiat: number;
+    transactions: number;
 }
 
 @Injectable({
@@ -16,13 +27,38 @@ export interface KeyPair {
 })
 export class BitcoinService {
 
+    wallet: Wallet[] = [];
 
     constructor() {
-        this.generateBrainWallet("password");
+        this.wallet = this.generateBip32Wallet('praise you muffin lion enable neck grocery crumble super myself license ghost');
+        console.log(this.wallet);
     }
 
 
-    async generateSHA265HashBuffer(input: string): Promise<Buffer> {
+    private getAddress(
+        publicKey: Buffer, 
+        network: Network = bitcoin.networks.bitcoin, 
+        segwit: Boolean = false
+    ): string {
+        return segwit ?
+            bitcoin.payments.p2wpkh({ pubkey: publicKey, network: network }).address! :
+            bitcoin.payments.p2pkh({ pubkey: publicKey, network: network }).address!
+    }
+
+
+    private deriveKeyPair(
+        root: BIP32Interface, 
+        path: string, 
+        addressPath: number
+    ): KeyPair {
+        path = path.concat(addressPath.toString());
+        const privKey = root.derivePath(path).privateKey!;
+        const ecPair = ECPair.fromPrivateKey(privKey);
+        return this.generateKeyPairFromEcPair(ecPair);
+    }
+
+
+    private async generateSHA265HashBuffer(input: string): Promise<Buffer> {
         const textAsBuffer = new TextEncoder().encode(input);
         const hashBuffer = await window.crypto.subtle.digest("SHA-256", textAsBuffer);
         const buffer = Buffer.from(hashBuffer);
@@ -32,17 +68,11 @@ export class BitcoinService {
 
     private generateKeyPairFromEcPair(
         ecPair: ECPairInterface,
-        segwit: boolean,
+        segwit: boolean = false,
         network: Network = bitcoin.networks.bitcoin
     ): KeyPair {
         const publicKey = ecPair.publicKey;
-        const address = segwit ?
-            bitcoin.payments.p2wpkh({ pubkey: publicKey, network: network }).address :
-            bitcoin.payments.p2pkh({ pubkey: publicKey, network: network }).address
-
-        if (address === undefined) {
-            throw new Error("No address");
-        }
+        const address = this.getAddress(publicKey, network, segwit);
 
         const keyPair: KeyPair = {
             address: address,
@@ -73,6 +103,17 @@ export class BitcoinService {
     }
 
 
+    generateKeyPairFromBuffer(
+        buffer: Buffer, 
+        segwit: boolean = false, 
+        compressed: boolean = false
+    ): KeyPair {
+        const options = { compressed: compressed };
+        const ecPair = ECPair.fromPrivateKey(buffer, options);
+        return this.generateKeyPairFromEcPair(ecPair, segwit);
+    }
+
+
     generateBrainWallet(
         password: string, 
         segwit: boolean = false, 
@@ -83,6 +124,49 @@ export class BitcoinService {
             const ecPair = ECPair.fromPrivateKey(hashBuffer, options);
             console.log(this.generateKeyPairFromEcPair(ecPair, segwit));
         })
+    }
+
+
+    generateBip32Wallet(
+        mnemonic?: string, 
+        totalAddresses: number = 20
+    ): Wallet[] {
+        let wallet: Wallet[] = [];
+        mnemonic = mnemonic ?? this.generateMnemonic();
+
+        const seed = this.generateSeedFromMnemonic(mnemonic);
+        const root = bip32.fromSeed(seed);
+
+        for (let x=0; x<totalAddresses; x++) {
+            wallet.push(this.setWallet("receiving", this.deriveKeyPair(root, "m/0'/0/", x)));
+        }
+
+        for (let x=0; x<10; x++) {
+            wallet.push(this.setWallet("change", this.deriveKeyPair(root, "m/0'/1/", x)));
+        }
+
+        return wallet;
+    }
+
+    
+    private setWallet(type: string, pair: KeyPair, balance: number = 0.00000001, fiat: number = 0, tx: number = 0): Wallet {
+        return {
+            addressType: type,
+            keyPair: pair,
+            balance: balance,
+            balanceFiat: fiat,
+            transactions: tx
+        }
+    }
+
+
+    generateMnemonic(): string {
+        return bip39.generateMnemonic();
+    }
+
+
+    generateSeedFromMnemonic(mnemonic: string): Buffer {
+        return bip39.mnemonicToSeedSync(mnemonic);
     }
 
 }
