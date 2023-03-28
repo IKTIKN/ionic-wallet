b086c11b9c1e4842a28ca52d0de97fc66f7f838f
IKTIKN <developermarc@outlook.com>
2023-03-28 22:04:28 +0200

    Spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { WalletService } from './services/wallet/wallet.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+	constructor(public wallet: WalletService) {
+		this.wallet.initWallet();
+	}
 }
