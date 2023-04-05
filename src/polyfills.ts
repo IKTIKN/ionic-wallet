794e4abc9f06b76dcf4e20cff7eca991e194b614
IKTIKN <developermarc@outlook.com>
2023-04-05 22:54:00 +0200

    Dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,3 +51,7 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import { Buffer } from 'buffer';
+
+(window as any).global = window;
+global.Buffer = Buffer;
